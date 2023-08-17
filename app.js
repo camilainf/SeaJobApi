0534a6de0ee00c6d0378eb338c24df52bc4f9102
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2023-08-17 15:07:52 -0400

    cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const initDb = require('./src/configs/db')
 const allRoutes = require("./src/routes/index");
+const cors = require('cors');
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 9000;
 
 // middleware
