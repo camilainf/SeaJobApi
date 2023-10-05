13a4b9c91e21b8d0cf4e90ece30452a000ea291c
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2023-10-05 14:53:36 -0300

    feat(Servicio): obtener servicios por categoria

--- src/routes/service.js
+++ src/routes/service.js
@@ -4,5 +4,6 @@ const serviceController = require('../controllers/service');
 
 router.post('/', serviceController.createService);
 router.get('/', serviceController.getAllServices);
+router.get('/byCategory', serviceController.getServicesByCategory);
 
 module.exports = router;
