77fed0b0c089cdb0cdd6db9c14798e87b4d9b51c
Sebastian <sebastian.moyano.r@mail.pucv.cl>
2023-10-13 00:27:33 -0300

    aaaaaa

--- src/controllers/offer.js
+++ src/controllers/offer.js
@@ -55,23 +55,49 @@ getOfferOfService = async (req, res) => {
 getOfferAceptedOfService = async (req, res) => {
   const { id } = req.params;
   try {
-    const offers = await Offer.find({
+    const offer = await Offer.findOne({
       idServicio: id,
       estaEscogida: true,
     });
-    res.json(offers);
+    if (offer) {
+      res.json(offer);
+    } else {
+      res.status(404).json({ message: "Oferta no encontrada" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 acceptAnOffer = async (req, res) => {
   const { id } = req.params;
+
   try {
+    // 1. Marcar la oferta como "aceptada"
     const offer = await Offer.findById(id);
+    if (!offer) {
+      throw new Error("Oferta no encontrada.");
+    }
     offer.estaEscogida = true;
-    const savedOffer = await offer.save();
-    res.status(201).json(savedOffer);
+    await offer.save();
+
+    // 2. Cambiar el estado del servicio relacionado con esa oferta
+    // Supongo que un estado '1' significa que la oferta ha sido aceptada. Cambia esto según tu lógica.
+    const service = await Service.findById(offer.idServicio);
+    if (!service) {
+      throw new Error("Servicio no encontrado.");
+    }
+    service.estado = 2;
+
+    // 3. Actualizar el monto del servicio con el monto ofertado
+    service.monto = offer.montoOfertado;
+
+    await service.save();
+
+
+    res.status(201).json({ message: "Oferta aceptada y servicio actualizado correctamente." });
   } catch (error) {
+    a
     res.status(500).json({ message: error.message });
   }
 };
